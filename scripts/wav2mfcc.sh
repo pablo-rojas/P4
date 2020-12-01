a18d937565dca7000ac01219743f44b2f33ebebc
Will-Arias <ariasruesga@gmail.com>
2020-12-01 18:48:06 +0100

    20201201

--- scripts/wav2mfcc.sh
+++ scripts/wav2mfcc.sh
@@ -41,10 +41,10 @@ fi
 
 # Main command for feature extration
 sox $inputfile -t raw -e signed -b 16 - | $X2X +sf | $FRAME -l 240 -p 80 | $WINDOW -l 240 -L 240 |
-	$MFCC -a 0.97 -c 22 -e 1 -s 16 -l 240 -L 256 -m $mfcc_order -n $mfcc_channelOrder -w 1 > $base.lp
+	$MFCC -a 0.97 -c 22 -e 1 -s 8 -l 240 -L 256 -m $mfcc_order -n $mfcc_channelOrder -w 1 > $base.lp
 
 # Our array files need a header with the number of cols and rows:
-ncol=$((lpc_order+1)) # lpc p =>  (gain a1 a2 ... ap) 
+ncol=$((mfcc_order)) # lpc p =>  (gain a1 a2 ... ap) 
 nrow=`$X2X +fa < $base.lp | wc -l | perl -ne 'print $_/'$ncol', "\n";'`
 
 # Build fmatrix file by placing nrow and ncol in front, and the data after them
